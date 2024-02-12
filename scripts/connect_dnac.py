e5aaa01b33b64555d56c3ae8edfafbddfbde82e1
Mark Coleman <mrmrcoleman@gmail.com>
2024-02-12 17:13:19 +0100

    return statement needed?

--- scripts/connect_dnac.py
+++ scripts/connect_dnac.py
@@ -3,15 +3,15 @@ This script connects to Cisco DNAC and returns a list of the available devices.
 
 It expects to find the following environment variables present and will exit if both are not set in the environment:
 
-DNAC_USERNAME
-DNAC_PASSWORD
+$DNAC_USERNAME
+$DNAC_PASSWORD
 """
 
 #import requests
 #import base64
 #import json
 #import urllib3
-import os
+#import os
 #import sys
 
 # Hack to get this working locally
@@ -76,3 +76,5 @@ class GetDNACDevices(Script):
             print(json.dumps(device_information, indent=4))
         except Exception as e:
             print(e)
+
+        return
