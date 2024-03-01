0ca134325033fc71757abbe054ec578eae3cd579
Mark Coleman <mcoleman@netboxlabs.com>
2024-03-01 13:21:14 +0000

    Added options

--- scripts/trigger_events.py
+++ scripts/trigger_events.py
@@ -1,12 +1,12 @@
 """
-Sends a webhook to the webhook handler for the event driven webinar
+Sends a webhook to the webhook handler for the Event Driven Webinar
 
 """
 
 import requests
 import urllib3
 
-from extras.scripts import Script
+from extras.scripts import Script, StringVar
 
 class TriggerEvents(Script):
 
@@ -22,13 +22,23 @@ class TriggerEvents(Script):
         name = "TriggerEvents"
         description = "Send webhooks to control the Event Driven Webinar"
 
+    webhook_endpoint = StringVar(label="Webhook Endpoint", required=True)
+    action = StringVar(label="Event Action", required=True)
+
     def run(self, data, commit):
+
+        webhook_endpoint = data["webhook_endpoint"]
+        payload = {
+            'action': data["action"],
+        }
+
+        self.log_success(f"Sending webhook to {webhook_endpoint} with payload: {payload}")
 
         # Suppress InsecureRequestWarning
         urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
         # Send a POST request to the webhook URL with the payload
-        response = requests.post(self.target_url, json=self.payload)
+        response = requests.post(webhook_endpoint, json=payload)
 
         # Check if the request was successful (HTTP status code 200)
         if response.status_code == 200:
